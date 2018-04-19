6fc812beda15af15b2bd40b1dc54541c761f6326
hyunwoo <chzhqk1994@gmail.com>
2018-04-20 01:11:31 +0900

    init

---
+++ mid_test/src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -0,0 +1,30 @@
+package kr.ac.jejunu;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProductDaoTest {
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        ProductDao productDao = new ProductDao();
+        Long id = 1L;
+        String title = "제주감귤";
+        Integer price = 15000;
+
+        Product product = productDao.get(id);
+        assertEquals(id, product.getId());
+        assertEquals(title, product.getTitle());
+        assertEquals(price, product.getPrice());
+    }
+}
